35918c93650349870f2b07d8f221337959d07ed7
robrod7 <robertreynoldscourse@gmail.com>
2026-04-07 21:36:29 -0300

    Ultima escrita

--- aula04/atividade02.py
+++ aula04/atividade02.py
@@ -1,7 +1,9 @@
 valor_compra = float(input('Digite o valor da compra: '))
 pagamento = input('Digite a forma de pagamento: ').lower() #.upper()
 
-if valor_compra > 250.0 and pagamento == 'vista':
+if valor_compra > 250.0 and pagamento == 'à vista':
     print(f'O valor final da compra é: {valor_compra*(1-0.16):.2f}')
 else:
     print(f'O valor de compra é {valor_compra:.2f}')
+
+###https://faetecrj.org/www-faetec-rj-gov-br-inscricao?gad_source=1&gad_campaignid=1967803879&gbraid=0AAAAADxWAGzAFSlOa2cvdAgBzdpRqLpUD&gclid=EAIaIQobChMIhYyFi_PckwMVTlpIAB1dzAh6EAAYASAAEgI9L_D_BwE
